9911116c7d1490614267bf3122e4f42c6ddb13ce
Bruno Miguel Barros Pinto <brunomiguelbarrospinto@gmail.com>
2025-02-07 15:52:50 +0100

    add DocumentModel and tests; configure coverage and testing scripts

---
+++ src/modules/documents/domain/DocumentModel.spec.ts
@@ -0,0 +1,17 @@
+import { DocumentModel, createDocumentMock } from "./DocumentModel";
+import { expect, test } from "vitest";
+
+test("should create a DocumentModel instance", () => {
+  const document = createDocumentMock({ Title: "Bruno" });
+  const documentModel = new DocumentModel(document);
+
+  expect(documentModel.id).toBe(document.ID);
+  expect(documentModel.title).toBe(document.Title);
+  expect(documentModel.contributors).toEqual(
+    document.Contributors.map((contributor) => contributor.Name)
+  );
+  expect(documentModel.version).toBe(document.Version);
+  expect(documentModel.attachment).toEqual(document.Attachments);
+  expect(documentModel.createdAt).toEqual(document.CreatedAt);
+  expect(documentModel.updatedAt).toEqual(document.UpdatedAt);
+});
---
+++ src/modules/documents/domain/DocumentModel.ts
@@ -0,0 +1,51 @@
+import { generateMock } from "@anatine/zod-mock";
+import { z } from "zod";
+
+// Define the schema for the document
+export const DocumentSchema = z.object({
+  ID: z.string(),
+  Title: z.string(),
+  Contributors: z.array(
+    z.object({
+      ID: z.string(),
+      Name: z.string(),
+    })
+  ),
+  Version: z.number(),
+  Attachments: z.array(z.string()),
+  UpdatedAt: z.date(),
+  CreatedAt: z.date(),
+});
+
+// Create a mock document
+export function createDocumentMock(
+  overwrite?: Partial<z.infer<typeof DocumentSchema>>
+) {
+  return { ...generateMock(DocumentSchema), ...overwrite };
+}
+
+// Define the document type
+export type Document = z.infer<typeof DocumentSchema>;
+
+// Define the document model
+export class DocumentModel {
+  public id: string;
+  public title: string;
+  public contributors: string[];
+  public version: number;
+  public attachment: string[];
+  public createdAt: Date;
+  public updatedAt: Date;
+
+  constructor(document: Document) {
+    this.id = document.ID;
+    this.title = document.Title;
+    this.contributors = document.Contributors.map(
+      (contributor) => contributor.Name
+    );
+    this.version = document.Version;
+    this.attachment = document.Attachments;
+    this.createdAt = document.CreatedAt;
+    this.updatedAt = document.UpdatedAt;
+  }
+}
